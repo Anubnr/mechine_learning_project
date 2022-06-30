a3ff3437d1344e00450c3045e5456d2bee6e8b4f
anusha <anushakethanaboina@gmail.com>
2022-06-30 10:51:35 +0530

    CI CD pipeline has been created

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app=Flask(__name__)
 
 @app.route("/", methods=['GET','POST'])
 def index():
-    return "Starting Mechine Learning Project"
+    return "CI CD pipeline has been established"
 
 
 if __name__=="__main__":
-    app.run(debug=True),
+    app.run(debug=True)
