a377160488022a64cbfc22761fa96da0f729cab1
anusha <anushakethanaboina@gmail.com>
2022-06-30 13:45:44 +0530

    first mechine learning project

--- setup.py
+++ setup.py
@@ -1,34 +1,32 @@
-from setuptools import find_packages, setup
+from setuptools import setup,find_packages
 from typing import List
-# declaring variables for setup
-PROJECT_NAME = "housing-prediction"
-VERSION = "0.0.1"
-AUTHOR = "Anusha Kethanaboina"
-DESCRIPTION = "This is my first FSDS project"
-PACKAGES = ["housing"]
-REQUIREMENT_FILE_NAME = "requirements.txt"
+
+#Declaring variables for setup functions
+PROJECT_NAME="housing-predictor"
+VERSION="0.0.3"
+AUTHOR="Anusha Kethanaboina"
+DESRCIPTION="This is a first FSDS Nov batch Machine Learning Project"
+
+REQUIREMENT_FILE_NAME="requirements.txt"
 
 
-
-def get_requirements_list() -> List[str] :
+def get_requirements_list()->List[str]:
+    """
+   
+    Description: This function is going to return list of requirement 
+    mention in requirements.txt file
+    return This function is going to return a list which contain name 
+    of libraries mentioned in requirements.txt file
+    """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines().remove("-e")
-
-        """Description: This function is going to return the list of requirements mentioned in the requirements.txt
-
-returns: This function is going to return a list which contains name of libraries mentioned in the requirmnents.txt file
-"""
+        return requirement_file.readlines().remove("-e .")
 
 
 setup(
-    name = PROJECT_NAME,
-    version = VERSION,
-    author = AUTHOR,
-    description = DESCRIPTION,
-    packages = find_packages(),
-    install_requires= get_requirements_list()
+name=PROJECT_NAME,
+version=VERSION,
+author=AUTHOR,
+description=DESRCIPTION,
+packages=find_packages(), 
+install_requires=get_requirements_list()
 )
-
-
-
-   
